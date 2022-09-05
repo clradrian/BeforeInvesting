bb569534c8b09790e3149e78a90db599a4347a85
Adrian <adrian2909@yahoo.com>
2022-09-05 17:19:32 +0300

    created more csv files with info

--- create_metadata.py
+++ create_metadata.py
@@ -16,10 +16,94 @@ class CreateMetadata:
     def create_basic_info_file(self):
         basic_info = self.get_all_info().info
         os.chdir(self.default_location)
-        print(f'{os.chdir(self.default_location)}', "----------")
         exported_info_location = check_and_create_folder("\exported_info")
         os.chdir(exported_info_location)
         company_ticker_folder = check_and_create_folder(f"\{self.company_ticker}")
         os.chdir(company_ticker_folder)
         with open(f"{self.company_ticker}_basic_info.json", "w") as outfile:
             json.dump(basic_info, outfile)
+
+
+    def create_financials_info_file(self):
+        financials_info = self.get_all_info().financials
+        financials_info.columns = financials_info.columns.astype(str).str.replace(' ', '_')
+        os.chdir(self.default_location)
+        exported_info_location = check_and_create_folder("\exported_info")
+        os.chdir(exported_info_location)
+        company_ticker_folder = check_and_create_folder(f"\{self.company_ticker}")
+        os.chdir(company_ticker_folder)
+        financials_info.to_csv(f"{self.company_ticker}_financials_info.csv")
+
+    def create_quarterly_financials_info_file(self):
+        financials_info = self.get_all_info().quarterly_financials
+        os.chdir(self.default_location)
+        exported_info_location = check_and_create_folder("\exported_info")
+        os.chdir(exported_info_location)
+        company_ticker_folder = check_and_create_folder(f"\{self.company_ticker}")
+        os.chdir(company_ticker_folder)
+        financials_info.to_csv(f"{self.company_ticker}_quarterly_financials_info.csv")
+
+
+    def create_balance_sheet_info_file(self):
+        financials_info = self.get_all_info().balance_sheet
+        os.chdir(self.default_location)
+        exported_info_location = check_and_create_folder("\exported_info")
+        os.chdir(exported_info_location)
+        company_ticker_folder = check_and_create_folder(f"\{self.company_ticker}")
+        os.chdir(company_ticker_folder)
+        financials_info.to_csv(f"{self.company_ticker}_balance_sheet_info.csv")
+
+    def create_quarterly_balance_sheet_info_file(self):
+        financials_info = self.get_all_info().quarterly_balance_sheet
+        os.chdir(self.default_location)
+        exported_info_location = check_and_create_folder("\exported_info")
+        os.chdir(exported_info_location)
+        company_ticker_folder = check_and_create_folder(f"\{self.company_ticker}")
+        os.chdir(company_ticker_folder)
+        financials_info.to_csv(f"{self.company_ticker}_quarterly_balance_sheet_info.csv")
+
+    def create_cashflow_info_file(self):
+        financials_info = self.get_all_info().cashflow
+        os.chdir(self.default_location)
+        exported_info_location = check_and_create_folder("\exported_info")
+        os.chdir(exported_info_location)
+        company_ticker_folder = check_and_create_folder(f"\{self.company_ticker}")
+        os.chdir(company_ticker_folder)
+        financials_info.to_csv(f"{self.company_ticker}_cashflow_info.csv")
+
+    def create_quarterly_cashflow_info_file(self):
+        financials_info = self.get_all_info().quarterly_cashflow
+        os.chdir(self.default_location)
+        exported_info_location = check_and_create_folder("\exported_info")
+        os.chdir(exported_info_location)
+        company_ticker_folder = check_and_create_folder(f"\{self.company_ticker}")
+        os.chdir(company_ticker_folder)
+        financials_info.to_csv(f"{self.company_ticker}_quarterly_cashflow_info.csv")
+
+
+    def create_earnings_info_file(self):
+        financials_info = self.get_all_info().earnings
+        os.chdir(self.default_location)
+        exported_info_location = check_and_create_folder("\exported_info")
+        os.chdir(exported_info_location)
+        company_ticker_folder = check_and_create_folder(f"\{self.company_ticker}")
+        os.chdir(company_ticker_folder)
+        financials_info.to_csv(f"{self.company_ticker}_earnings_info.csv")
+
+    def create_quarterly_earnings_info_file(self):
+        financials_info = self.get_all_info().quarterly_earnings
+        os.chdir(self.default_location)
+        exported_info_location = check_and_create_folder("\exported_info")
+        os.chdir(exported_info_location)
+        company_ticker_folder = check_and_create_folder(f"\{self.company_ticker}")
+        os.chdir(company_ticker_folder)
+        financials_info.to_csv(f"{self.company_ticker}_quarterly_earnings_info.csv")
+
+    def create_recommendations_info_file(self):
+        financials_info = self.get_all_info().recommendations
+        os.chdir(self.default_location)
+        exported_info_location = check_and_create_folder("\exported_info")
+        os.chdir(exported_info_location)
+        company_ticker_folder = check_and_create_folder(f"\{self.company_ticker}")
+        os.chdir(company_ticker_folder)
+        financials_info.to_csv(f"{self.company_ticker}_recommendations_info.csv")
--- get_financial.py
+++ get_financial.py
@@ -2,24 +2,17 @@ import yfinance as yf
 import pandas as pd
 import matplotlib.pyplot as plt
 from numerize import numerize
-# from matplotlib import style
-
+from utils import calculate_long_numbers
+from datetime import datetime
 
 class GetFinancials:
     def __init__(self, company_ticker):
         self.company_ticker = company_ticker
 
-    def get_financial_data_for_ticker(self):
-        stock_ticker = yf.Ticker(self.company_ticker)
-        return stock_ticker.financials
-
-    def calculate_long_numbers(self, long_number):
-        return numerize.numerize(long_number)
-
     def get_net_income_df(self, column_name):
-        get_df_financials = self.get_financial_data_for_ticker()
-        t_get_df_financials = get_df_financials.T
-        get_net_income_column = t_get_df_financials[[column_name]]
+        df_financials = pd.read_csv(r"C:\Users\Chelariu's\PycharmProjects\git\git_BeforeInvesting\BeforeInvesting\exported_info\MSFT\MSFT_financials_info.csv", index_col=0)
+        t_get_df_financials = df_financials.T
+        get_net_income_column = t_get_df_financials[column_name]
         return get_net_income_column
 
     def change_axis(self, column_name="Net Income"):
@@ -27,12 +20,12 @@ class GetFinancials:
         date_axis = []
         for date in list(df_net_income.index.values):
             new_date = pd.to_datetime(date)
-            # str(new_date.date())
-            date_axis.append(str(new_date.date()))
+            datetime_object = datetime.strptime(str(new_date.date()), "%Y-%m-%d").strftime('%b-%Y')
+            date_axis.append(datetime_object)
 
         transformed_values = []
-        for x in df_net_income[column_name].head():
-            new_values = self.calculate_long_numbers(x)
+        for x in df_net_income.head():
+            new_values = calculate_long_numbers(x)
             transformed_values.append(new_values)
         return sorted(date_axis), sorted(transformed_values)
 
@@ -41,12 +34,13 @@ class GetFinancials:
         int_list = []
         for value in y_axis:
             delete_last_ch = value[:-1]
+            get_last_ch = value[-1]
             int_list.append(float(delete_last_ch))
         plt.style.use('dark_background')
         plt.bar(x_axis, int_list, color='green',
-                width=0.5)
-        plt.margins(0.3, 0.3)
-        plt.xlabel("Date")
-        plt.ylabel("Net Income")
+                width=0.6)
+        plt.margins(0.1, 0.1)
+        # plt.xlabel("Date")
+        plt.ylabel(f"Net Income ({get_last_ch})")
         plt.title(f"{self.company_ticker} Net Income")
         plt.show()
